cf99ce8b7a250279612c16d417fc69ba3558c0ef
Mukkawar-Shlok <87766297+Mukkawar-Shlok@users.noreply.github.com>
2022-03-14 17:55:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var num = document.querySelectorAll(".button").length;
+
+for (var j = 0; j < num; j++) {
+    document.querySelectorAll(".button")[j]
+        .addEventListener("click", function () {
+            var b = this.innerHTML;
+            sound(b);
+        }
+        );
+}
+
+document.addEventListener("keypress", function (event) {
+    sound(event.key);
+}
+);
+
+function sound(key) {
+    switch (key) {
+        case "a":
+            var sound1 = new Audio("/sounds/crash.mp3");
+            sound1.play();
+            break;
+
+        case "s":
+            var sound2 = new Audio("sounds/kick-bass.mp3");
+            sound2.play();
+            break;
+
+        case "d":
+            var sound3 = new Audio('sounds/snare.mp3');
+            sound3.play();
+            break;
+
+        case "f":
+            var sound4 = new Audio('sounds/tom-1.mp3');
+            sound4.play();
+            break;
+
+        case "g":
+            var sound5 = new Audio('sounds/tom-2.mp3');
+            sound5.play();
+            break;
+
+        case "h":
+            var sound6 = new Audio('sounds/tom-3.mp3');
+            sound6.play();
+            break;
+
+
+        default: console.log(key);
+    }
+}
